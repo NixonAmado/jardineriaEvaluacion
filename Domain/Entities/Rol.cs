a35530c0730cd85b2b47388f9994739d1793ad5d
nixonamado <nixonamado00@gmail.com>
2023-10-14 22:07:42 -0500

    refactor: :fire: Eliminacion de duplicados
    Se han eliminados las entidades duplicadas de JWT

--- Domain/Entities/Rol.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Entities;
-
-public class Rol : BaseEntity
-{
-    string Name {get;set;}
-    public ICollection<User> Users {get;set;} = new HashSet<User>();
-    public ICollection<UserRol> UsersRols {get;set;}
-}
--- Domain/Entities/UserRol.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities;
-
-public class UserRol
-{
-    public int IdUserFk {get;set;}
-    public User User {get;set;}
-    public int IdRolFk {get;set;}
-    public Rol Rol {get;set;}
-}
